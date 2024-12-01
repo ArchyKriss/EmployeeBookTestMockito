ac0638a4a752f837aa63761c0d8ce1c120933519
MSI <loreleicruellg@gmail.com>
2024-12-01 14:50:25 +0300

    initial commit

---
+++ src/main/java/com/example/EmployeeBookTestMockito/EmployeeBookTestMockitoApplication.java
@@ -0,0 +1,13 @@
+package com.example.EmployeeBookTestMockito;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeBookTestMockitoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeBookTestMockitoApplication.class, args);
+	}
+
+}
